db403c4c5bbe7ff5d0abf0f3a1df50c266148965
thibautGabette <thibaut.gabette@u-bourgogne.fr>
2015-12-14 14:18:06 +0100

    v2.0

--- LibreAudioView/openCV300/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ LibreAudioView/openCV300/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /home/thibautg/boulot/dev/AndroidStudio/Applis de Test/LibreAudioView_test_openCv300/openCV300/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /home/thibautg/boulot/dev/AndroidStudio/projet/LibreAudioView/openCV300/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
